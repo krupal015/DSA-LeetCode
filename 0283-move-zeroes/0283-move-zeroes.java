89b78d2b8d6e24986384faf1605f7dae1e52efca
krupal015 <patelkrupald2324@gmail.com>
2026-05-17 19:56:15 +0530

    Time: 2 ms (91.68%), Space: 47.6 MB (74.61%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int j =0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0 ){
+                swap(nums,i,j);
+                j++;
+                
+            }
+        }
+    }
+    void swap(int arr[],int a,int b){
+        int temp =arr[a];
+        arr[a]=arr[b];
+        arr[b]=temp; 
+    }
+}
